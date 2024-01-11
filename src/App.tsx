d5efa28d795e1a32492abaa8607b92d978cbfad9
Abu Bakkar <abu.bakar@attech-ltd.com>
2024-01-12 00:32:05 +0500

    add line, pencil and rect functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { MouseEvent, useLayoutEffect, useState } from "react";
+import { MouseEvent, useEffect, useLayoutEffect, useState } from "react";
 import rough from "roughjs";
+import getStroke from "perfect-freehand";
+import { useHistory } from "./useHistory";
 
-type ElementType = {
+type SelectedElementType = ElementType & {
+  xOffsets?: number[];
+  yOffsets?: number[];
+  offsetX?: number;
+  offsetY?: number;
+};
+interface ExtendedElementType extends ElementType {
+  xOffsets?: number[];
+  yOffsets?: number[];
+}
+export type ElementType = {
   id: number;
   x1: number;
   y1: number;
@@ -13,20 +25,85 @@ type ElementType = {
   roughElement: any;
   offsetX?: number;
   offsetY?: number;
+  position?: string | null;
+  points?: { x: number; y: number }[];
 };
 
 enum Tools {
-  Selection = "selection",
+  Pencil = "pencil",
   Line = "line",
   Rectangle = "rectangle",
+  Selection = "selection",
 }
 
 export default function App() {
-  const [elements, setElements] = useState<ElementType[]>([]);
+  const { elements, setElements, undo, redo } = useHistory([]);
   const [action, setAction] = useState("none");
   const [tool, setTool] = useState<Tools>(Tools.Line);
   const [selectedElement, setSelectedElement] = useState<ElementType | null>();
   const generator = rough.generator();
+
+  const cursorForPosition = (position: string) => {
+    switch (position) {
+      case "topLeft":
+      case "bottomRight":
+        return "nwse-resize";
+      case "topRight":
+      case "bottomLeft":
+        return "nesw-resize";
+      case "start":
+      case "end":
+        return "move";
+      case "inside":
+        return "move";
+      default:
+        return "default";
+    }
+  };
+
+  const resizedCoordinates = (
+    clientX: number,
+    clientY: number,
+    position: string,
+    coordinates: { x1: number; y1: number; x2: number; y2: number }
+  ) => {
+    const { x1, y1, x2, y2 } = coordinates;
+
+    switch (position) {
+      case "start":
+      case "topLeft":
+        return {
+          x1: clientX,
+          y1: clientY,
+          x2,
+          y2,
+        };
+      case "topRight":
+        return {
+          x1,
+          y1: clientY,
+          x2: clientX,
+          y2,
+        };
+      case "bottomLeft":
+        return {
+          x1: clientX,
+          y1,
+          x2,
+          y2: clientY,
+        };
+      case "end":
+      case "bottomRight":
+        return {
+          x1,
+          y1,
+          x2: clientX,
+          y2: clientY,
+        };
+      default:
+        return coordinates;
+    }
+  };
 
   const createElement = (
     id: number,
@@ -36,11 +113,31 @@ export default function App() {
     y2: number,
     type: Tools
   ): ElementType => {
-    const roughElement =
-      type === Tools.Line
-        ? generator.line(x1, y1, x2, y2)
-        : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-    return { id, x1, y1, x2, y2, type, roughElement };
+    switch (type) {
+      case Tools.Line:
+      case Tools.Rectangle: {
+        const roughElement =
+          type === Tools.Line
+            ? generator.line(x1, y1, x2, y2)
+            : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+        return { id, x1, y1, x2, y2, type, roughElement };
+      }
+      case Tools.Pencil: {
+        const defaultRoughElement = null;
+        return {
+          id,
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 0,
+          type,
+          points: [{ x: x1, y: y1 }],
+          roughElement: defaultRoughElement,
+        };
+      }
+      default:
+        throw new Error(`Type not recognised: ${type}`);
+    }
   };
 
   type Point = { x: number; y: number };
@@ -48,7 +145,70 @@ export default function App() {
   const distance = (a: Point, b: Point) =>
     Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 
-  const isWithinElement = (x: number, y: number, element: ElementType) => {
+  const nearPoint = (
+    x: number,
+    y: number,
+    x1: number,
+    y1: number,
+    name: string
+  ) => {
+    return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
+  };
+
+  const onLine = (
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    x: number,
+    y: number,
+    maxDistance: number = 1
+  ): string | null => {
+    const a: Point = { x: x1, y: y1 };
+    const b: Point = { x: x2, y: y2 };
+    const c: Point = { x, y };
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+    return Math.abs(offset) < maxDistance ? "inside" : null;
+  };
+
+  const positionWithinElement = (
+    x: number,
+    y: number,
+    element: ElementType
+  ) => {
+    const { type, x1, x2, y1, y2 } = element;
+    switch (type) {
+      case Tools.Line: {
+        const on = onLine(x1, y1, x2, y2, x, y);
+        const start = nearPoint(x, y, x1, y1, "start");
+        const end = nearPoint(x, y, x2, y2, "end");
+        return start || end || on;
+      }
+      case Tools.Rectangle: {
+        const topLeft = nearPoint(x, y, x1, y1, "topLeft");
+        const topRight = nearPoint(x, y, x2, y1, "topRight");
+        const bottomLeft = nearPoint(x, y, x1, y2, "bottomLeft");
+        const bottomRight = nearPoint(x, y, x2, y2, "bottomRight");
+        const inside =
+          x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
+        return topLeft || topRight || bottomLeft || bottomRight || inside;
+      }
+      case Tools.Pencil: {
+        const betweenAnyPoint = element.points!.some((point, index) => {
+          const nextPoint = element.points![index + 1];
+          if (!nextPoint) return false;
+          return (
+            onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y, 5) != null
+          );
+        });
+        return betweenAnyPoint ? "inside" : null;
+      }
+      default:
+        throw new Error(`Type not recognised: ${type}`);
+    }
+  };
+
+  const adjustElementCoordinates = (element: ElementType) => {
     const { type, x1, y1, x2, y2 } = element;
 
     if (type === Tools.Rectangle) {
@@ -56,14 +216,13 @@ export default function App() {
       const maxX = Math.max(x1, x2);
       const minY = Math.min(y1, y2);
       const maxY = Math.max(y1, y2);
-      return x >= minX && x <= maxX && y >= minY && y <= maxY;
+      return { x1: minX, y1: minY, x2: maxX, y2: maxY };
     } else {
-      // 💭 Source: https://stackoverflow.com/questions/17692922/check-is-a-point-x-y-is-between-two-points-drawn-on-a-straight-line/17693146#17693146
-      const a = { x: x1, y: y1 };
-      const b = { x: x2, y: y2 };
-      const c = { x, y };
-      const offset = distance(a, b) - (distance(a, c) + distance(b, c));
-      return Math.abs(offset) < 1;
+      if (x1 < x2 || (x1 === x2 && y1 < y2)) {
+        return { x1, y1, x2, y2 };
+      } else {
+        return { x1: x2, y1: y2, x2: x1, y2: y1 };
+      }
     }
   };
 
@@ -72,7 +231,74 @@ export default function App() {
     y: number,
     elements: ElementType[]
   ) => {
-    return elements.find((element) => isWithinElement(x, y, element));
+    return elements
+      .map((element) => ({
+        ...element,
+        position: positionWithinElement(x, y, element),
+      }))
+      .find((element) => element.position !== null);
+  };
+
+  const getSvgPathFromStroke = (stroke: [number, number][]) => {
+    if (!stroke.length) return "";
+
+    const d = stroke.reduce(
+      (
+        acc: string[],
+        [x0, y0]: [number, number],
+        i: number,
+        arr: [number, number][]
+      ) => {
+        const [x1, y1] = arr[(i + 1) % arr.length];
+        acc.push(
+          x0.toString(),
+          y0.toString(),
+          ((x0 + x1) / 2).toString(),
+          ((y0 + y1) / 2).toString()
+        );
+        return acc;
+      },
+      ["M", ...stroke[0].map((num) => num.toString()), "Q"]
+    );
+
+    d.push("Z");
+    return d.join(" ");
+  };
+
+  const drawElement = (
+    // TODO: add type
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    roughCanvas: any,
+    context: CanvasRenderingContext2D,
+    element: ElementType
+  ) => {
+    switch (element.type) {
+      case "line":
+      case "rectangle":
+        roughCanvas.draw(element.roughElement);
+        break;
+      case "pencil": {
+        if (!element.points) {
+          throw new Error("Pencil element points are undefined");
+        }
+        const strokePoints = getStroke(element.points);
+        const formattedPoints: [number, number][] = strokePoints.map(
+          (point) => {
+            if (point.length !== 2) {
+              throw new Error(
+                `Expected point to have exactly 2 elements, got ${point.length}`
+              );
+            }
+            return [point[0], point[1]];
+          }
+        );
+        const stroke = getSvgPathFromStroke(formattedPoints);
+        context.fill(new Path2D(stroke));
+        break;
+      }
+      default:
+        throw new Error(`Type not recognised: ${element.type}`);
+    }
   };
 
   useLayoutEffect(() => {
@@ -82,10 +308,29 @@ export default function App() {
 
     const roughCanvas = rough.canvas(canvas);
 
-    elements.forEach(({ roughElement }) => {
-      roughCanvas.draw(roughElement);
-    });
+    elements.forEach((element) => drawElement(roughCanvas, context, element));
   }, [elements]);
+
+  useEffect(() => {
+    const undoRedoFunction = (event: KeyboardEvent) => {
+      if (event.ctrlKey || event.metaKey) {
+        if (event.key === "z") {
+          if (event.shiftKey) {
+            redo();
+          } else {
+            undo();
+          }
+        } else if (event.key === "y") {
+          redo();
+        }
+      }
+    };
+
+    document.addEventListener("keydown", undoRedoFunction);
+    return () => {
+      document.removeEventListener("keydown", undoRedoFunction);
+    };
+  }, [undo, redo]);
 
   const updateElement = (
     id: number,
@@ -95,27 +340,58 @@ export default function App() {
     y2: number,
     type: Tools
   ) => {
-    const updateElement = createElement(id, x1, y1, x2, y2, type);
-
     const elementsCopy = [...elements];
-    elementsCopy[id] = updateElement;
-    setElements(elementsCopy);
+    switch (type) {
+      case Tools.Line:
+      case Tools.Rectangle: {
+        elementsCopy[id] = createElement(id, x1, y1, x2, y2, type);
+        break;
+      }
+      case Tools.Pencil: {
+        const existingPoints = elementsCopy[id].points || [];
+        elementsCopy[id].points = [...existingPoints, { x: x2, y: y2 }];
+        break;
+      }
+      default:
+        throw new Error(`Type not recognised: ${type}`);
+    }
+    setElements(elementsCopy, true);
   };
+
+  const adjustmentRequired = (type: Tools) =>
+    ["line", "rectangle"].includes(type);
 
   const handleMouseDown = (event: MouseEvent<HTMLCanvasElement>) => {
     const { clientX, clientY } = event;
 
     if (tool === Tools.Selection) {
       const element = getElementAtPosition(clientX, clientY, elements);
+
       if (element) {
-        const offsetX = clientX - element.x1;
-        const offsetY = clientY - element.y1;
-        setSelectedElement({ ...element, offsetX, offsetY });
-        setAction("moving");
+        let selectedElement: SelectedElementType = { ...element };
+
+        if (element.type === "pencil" && element.points) {
+          const xOffsets = element.points.map((point) => clientX - point.x);
+          const yOffsets = element.points.map((point) => clientY - point.y);
+          selectedElement = { ...selectedElement, xOffsets, yOffsets };
+        } else {
+          const offsetX = clientX - selectedElement.x1;
+          const offsetY = clientY - selectedElement.y1;
+          selectedElement = { ...selectedElement, offsetX, offsetY };
+        }
+
+        setSelectedElement(selectedElement);
+        setElements((prevState) => prevState);
+
+        if (element.position === "inside") {
+          setAction("moving");
+        } else {
+          setAction("resizing");
+        }
       }
     } else {
       const id = elements.length;
-      const element = createElement(
+      const newElement = createElement(
         id,
         clientX,
         clientY,
@@ -123,7 +399,8 @@ export default function App() {
         clientY,
         tool
       );
-      setElements((prevState) => [...prevState, element]);
+      setElements((prevState) => [...prevState, newElement]);
+      setSelectedElement(newElement);
       setAction("drawing");
     }
   };
@@ -132,13 +409,15 @@ export default function App() {
     const { clientX, clientY } = event;
 
     if (tool === Tools.Selection) {
-      (event.target as HTMLElement).style.cursor = getElementAtPosition(
-        clientX,
-        clientY,
-        elements
-      )
-        ? "move"
-        : "default";
+      const element = getElementAtPosition(clientX, clientY, elements);
+
+      if (element && element.position) {
+        (event.target as HTMLElement).style.cursor = cursorForPosition(
+          element.position
+        );
+      } else {
+        (event.target as HTMLElement).style.cursor = "default";
+      }
     }
 
     if (action === "drawing") {
@@ -146,21 +425,69 @@ export default function App() {
       const { x1, y1 } = elements[index];
       updateElement(index, x1, y1, clientX, clientY, tool);
     } else if (action === "moving" && selectedElement) {
-      const { id, x1, x2, y1, y2, type, offsetX, offsetY } = selectedElement;
-      // 🌶️ Check if offsetX and offsetY are defined, and provide a fallback value of 0 if not
-      const safeOffsetX = offsetX ?? 0;
-      const safeOffsetY = offsetY ?? 0;
-      const newX1 = clientX - safeOffsetX;
-      const newY1 = clientY - safeOffsetY;
-      // 🫐 Calculate the new position for x2 and y2 based on the original size
-      const newX2 = newX1 + (x2 - x1);
-      const newY2 = newY1 + (y2 - y1);
+      if (
+        selectedElement.type === "pencil" &&
+        "points" in selectedElement &&
+        "xOffsets" in selectedElement &&
+        "yOffsets" in selectedElement
+      ) {
+        const extendedElement = selectedElement as ExtendedElementType;
+        const newPoints = extendedElement.points!.map((_, index) => ({
+          x: clientX - extendedElement.xOffsets![index],
+          y: clientY - extendedElement.yOffsets![index],
+        }));
+        const elementsCopy = [...elements];
+        elementsCopy[extendedElement.id] = {
+          ...elementsCopy[extendedElement.id],
+          points: newPoints,
+        };
+        setElements(elementsCopy, true);
+      } else {
+        const { id, x1, x2, y1, y2, type, offsetX, offsetY } =
+          selectedElement as ExtendedElementType;
+        const safeOffsetX = offsetX ?? 0;
+        const safeOffsetY = offsetY ?? 0;
+        const newX1 = clientX - safeOffsetX;
+        const newY1 = clientY - safeOffsetY;
+        // 🫐 Calculate the new position for x2 and y2 based on the original size
+        const newX2 = newX1 + (x2 - x1);
+        const newY2 = newY1 + (y2 - y1);
 
-      updateElement(id, newX1, newY1, newX2, newY2, type);
+        updateElement(id, newX1, newY1, newX2, newY2, type);
+      }
+    } else if (
+      action === "resizing" &&
+      selectedElement &&
+      selectedElement.position
+    ) {
+      const { id, type, position, ...coordinates } =
+        selectedElement as ExtendedElementType;
+
+      if (typeof position === "string") {
+        const { x1, y1, x2, y2 } = resizedCoordinates(
+          clientX,
+          clientY,
+          position,
+          coordinates
+        );
+        updateElement(id, x1, y1, x2, y2, type);
+      }
     }
   };
 
   const handleMouseUp = () => {
+    if (selectedElement) {
+      const index = selectedElement.id;
+      const { id, type } = elements[index];
+      if (
+        (action === "drawing" || action === "resizing") &&
+        adjustmentRequired(type)
+      ) {
+        const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
+        updateElement(id, x1, y1, x2, y2, type);
+      }
+    }
+
     setAction("none");
   };
 
@@ -195,6 +522,20 @@ export default function App() {
         />
 
         <label htmlFor="rectangle">rectangle</label>
+
+        <input
+          type="radio"
+          name="pencil"
+          id="pencil"
+          checked={tool === Tools.Pencil}
+          onChange={() => setTool(Tools.Pencil)}
+        />
+
+        <label htmlFor="pencil">pencil</label>
+      </div>
+      <div style={{ position: "fixed", zIndex: 2, bottom: 0, padding: 10 }}>
+        <button onClick={undo}>Undo</button>
+        <button onClick={redo}>Redo</button>
       </div>
       <canvas
         id="canvas"
